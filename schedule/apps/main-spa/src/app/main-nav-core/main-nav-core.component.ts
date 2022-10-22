02c25c6888029abb969b4b5013859c2a7fb8d2f2
kiIIer <39669501+kiIIer@users.noreply.github.com>
2022-10-22 17:31:20 +0200

    Initial setup (#2)
    * created project
    
    * navigation created

---
+++ schedule/apps/main-spa/src/app/main-nav-core/main-nav-core.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+@Component({
+  selector: 'schedule-main-nav-core',
+  templateUrl: './main-nav-core.component.html',
+  styleUrls: ['./main-nav-core.component.css']
+})
+export class MainNavCoreComponent {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+}
