0b58eac1c79bcf4e7a5656095a68025939f07b6b
killer <michael.molchanov.2004@gmail.com>
2022-10-22 17:30:57 +0200

    navigation created

--- schedule/apps/main-spa/src/app/app.module.ts
+++ schedule/apps/main-spa/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MainNavCoreComponent} from './main-nav-core/main-nav-core.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, MainNavCoreComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
