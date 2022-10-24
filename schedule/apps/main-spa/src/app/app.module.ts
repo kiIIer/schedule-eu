4ba4623dd8c6dff68fa6dba176e67c07071e3f4d
kiIIer <39669501+kiIIer@users.noreply.github.com>
2022-10-24 15:12:07 +0200

    router module (#5)

--- schedule/apps/main-spa/src/app/app.module.ts
+++ schedule/apps/main-spa/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MainNavCoreComponent} from './main-nav-core/main-nav-core.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
+import {RouterOutlet} from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent, MainNavCoreComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatButtonModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatButtonModule, RouterOutlet],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
