f8fb280223e32dde80ddfe68281dfcc4102af08d
Spectraza <savichpg@yahoo.com>
2022-10-29 16:12:55 +0300

    autogenerated component

--- schedule/apps/main-spa/src/app/app.module.ts
+++ schedule/apps/main-spa/src/app/app.module.ts
@@ -1,21 +1,30 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MainNavCoreComponent} from './main-nav-core/main-nav-core.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
-import {RouterOutlet} from '@angular/router';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavCoreComponent } from './main-nav-core/main-nav-core.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+import { ContactsComponent } from './presentaion/contacts/contacts.component';
 
 @NgModule({
-  declarations: [AppComponent, MainNavCoreComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule, MatIconModule, MatToolbarModule, MatListModule, MatButtonModule, RouterOutlet],
+  declarations: [AppComponent, MainNavCoreComponent, ContactsComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatButtonModule,
+    RouterOutlet,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
---
+++ schedule/apps/main-spa/src/app/presentaion/contacts/contacts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'schedule-contacts',
+  templateUrl: './contacts.component.html',
+  styleUrls: ['./contacts.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class ContactsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
