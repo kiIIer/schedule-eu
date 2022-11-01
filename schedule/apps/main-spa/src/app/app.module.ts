ab1a7f74ba7249fdd72cbc7c426d3f9b0e416b89
killer <michael.molchanov.2004@gmail.com>
2022-11-01 15:34:10 +0100

    app-module

--- schedule/apps/main-spa/src/app/app.module.ts
+++ schedule/apps/main-spa/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MainNavCoreComponent} from './main-nav-core/main-nav-core.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
-import {RouterOutlet} from '@angular/router';
-import {AppRoutingModule} from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavCoreComponent } from './main-nav-core/main-nav-core.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import * as fromContacts from './state/contacts/contacts.reducer';
+import { ContactsEffects } from './state/contacts/contacts.effects';
 
 @NgModule({
   declarations: [AppComponent, MainNavCoreComponent],
@@ -24,9 +30,25 @@ import {AppRoutingModule} from './app-routing.module';
     MatButtonModule,
     RouterOutlet,
     AppRoutingModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: !environment.production ? [] : [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    StoreModule.forFeature(
+      fromContacts.CONTACTS_FEATURE_KEY,
+      fromContacts.contactsReducer
+    ),
+    EffectsModule.forFeature([ContactsEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
