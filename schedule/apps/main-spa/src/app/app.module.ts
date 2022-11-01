33496e70287a132f6986f1804427650ea00d3125
killer <michael.molchanov.2004@gmail.com>
2022-11-01 15:44:14 +0100

    router state

--- schedule/apps/main-spa/src/app/app.module.ts
+++ schedule/apps/main-spa/src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavCoreComponent } from './main-nav-core/main-nav-core.component';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterOutlet } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MainNavCoreComponent} from './main-nav-core/main-nav-core.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterOutlet} from '@angular/router';
+import {AppRoutingModule} from './app-routing.module';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
 import * as fromContacts from './state/contacts/contacts.reducer';
-import { ContactsEffects } from './state/contacts/contacts.effects';
+import {ContactsEffects} from './state/contacts/contacts.effects';
 import * as fromSchedules from './state/schedules/schedules.reducer';
-import { SchedulesEffects } from './state/schedules/schedules.effects';
+import {SchedulesEffects} from './state/schedules/schedules.effects';
+import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store';
 
 @NgModule({
   declarations: [AppComponent, MainNavCoreComponent],
@@ -33,29 +34,32 @@ import { SchedulesEffects } from './state/schedules/schedules.effects';
     RouterOutlet,
     AppRoutingModule,
     StoreModule.forRoot(
-      {},
+      {
+        router: routerReducer,
+      },
       {
         metaReducers: !environment.production ? [] : [],
         runtimeChecks: {
           strictActionImmutability: true,
           strictStateImmutability: true,
         },
-      }
+      },
     ),
     EffectsModule.forRoot([]),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreModule.forFeature(
       fromContacts.CONTACTS_FEATURE_KEY,
-      fromContacts.contactsReducer
+      fromContacts.contactsReducer,
     ),
     EffectsModule.forFeature([ContactsEffects]),
     StoreModule.forFeature(
       fromSchedules.SCHEDULES_FEATURE_KEY,
-      fromSchedules.schedulesReducer
+      fromSchedules.schedulesReducer,
     ),
     EffectsModule.forFeature([SchedulesEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
