ed57f120c35a8808d6626a9780455babdad96078
killer <michael.molchanov.2004@gmail.com>
2022-11-03 23:22:42 +0100

    path to contacts

--- schedule/apps/main-spa/src/app/app-routing.module.ts
+++ schedule/apps/main-spa/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {ContactsCoreComponent} from './core/contacts-core/contacts-core.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'contacts', component: ContactsCoreComponent},
+];
 
 @NgModule({
   imports: [
