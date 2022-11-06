a04f37a8452f484042aa39cfe879147a1e476062
killer <michael.molchanov.2004@gmail.com>
2022-11-06 19:12:53 +0100

    router effect

--- schedule/apps/main-spa/src/app/main-nav/main-nav.component.ts
+++ schedule/apps/main-spa/src/app/main-nav/main-nav.component.ts
@@ -1,23 +1,24 @@
 import {Component, EventEmitter, Output} from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {Observable} from 'rxjs';
+import {map, shareReplay} from 'rxjs/operators';
 
 @Component({
   selector: 'schedule-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  styleUrls: ['./main-nav.component.css'],
 })
-export class MainNavCoreComponent {
+export class MainNavComponent {
 //TODO: Handle Event please '^'
   @Output() goEvent: EventEmitter<string> = new EventEmitter<string>();
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
-      shareReplay()
+      shareReplay(),
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
 
 }
--- schedule/apps/main-spa/src/app/state/router/app-router.effects.ts
+++ schedule/apps/main-spa/src/app/state/router/app-router.effects.ts
@@ -1,19 +1,21 @@
 import {Injectable} from '@angular/core';
 import {createEffect, Actions, ofType} from '@ngrx/effects';
 
-import {RouterModule} from '@angular/router';
+import {Router} from '@angular/router';
+import {goToUrl} from './app-router.actions';
+import {tap} from 'rxjs';
 
 @Injectable()
 export class AppRouterEffects {
 
   goto$ = createEffect(
     () => this.actions$.pipe(
-      ofType(RouterActions.routerGoTo),
+      ofType(goToUrl),
       tap((action) => this.router.navigateByUrl(action.url)),
     ),
     {dispatch: false},
   );
 
-  constructor(private readonly actions$: Actions, private router: RouterModule) {
+  constructor(private readonly actions$: Actions, private router: Router) {
   }
 }
