d0919e3d44871bfb55a2135f473d8ba2583770ac
kiIIer <39669501+kiIIer@users.noreply.github.com>
2022-11-19 17:07:34 +0100

    complete faculties done (#21)

--- schedule/apps/main-spa/src/app/state/faculties/faculties.effects.ts
+++ schedule/apps/main-spa/src/app/state/faculties/faculties.effects.ts
@@ -4,10 +4,30 @@ import {fetch} from '@nrwl/angular';
 
 import * as FacultiesActions from './faculties.actions';
 import * as FacultiesFeature from './faculties.reducer';
+import {SheetWorkerService} from '../../services/sheet-worker/sheet-worker.service';
+import {FacultiesEntity} from './faculties.models';
+import {environment} from '../../../environments/environment';
+import {map, mergeMap} from 'rxjs/operators';
 
 @Injectable()
 export class FacultiesEffects {
+  private readonly facultiesLink: string = environment.faculties;
 
-  constructor(private readonly actions$: Actions) {
+  init$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(FacultiesActions.initFaculties),
+      mergeMap((action) => {
+        return this.sheetWorker.getSheet(this.facultiesLink, 'A:B').pipe(
+          map((response) =>
+            response.ok
+              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+              ? FacultiesActions.loadFacultiesSuccess({faculties: response.body!})
+              : FacultiesActions.loadFacultiesFailure({error: response.statusText})),
+        );
+      }),
+    ),
+  );
+
+  constructor(private readonly actions$: Actions, private sheetWorker: SheetWorkerService<FacultiesEntity>) {
   }
 }
