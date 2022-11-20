1824ba262d224d170eea310c80f1d678f7b381bf
killer <michael.molchanov.2004@gmail.com>
2022-11-20 16:00:38 +0100

    guard setup

--- schedule/apps/main-spa/src/app/app-routing.module.ts
+++ schedule/apps/main-spa/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import {RouterModule, Routes} from '@angular/router';
 import {StoreRouterConnectingModule} from '@ngrx/router-store';
 import {ContactsCoreComponent} from './core/contacts-core/contacts-core.component';
 import {ScheduleCoreComponent} from './core/schedule-core/schedule-core.component';
+import {ContactsGuard} from './guard/contacts.guard';
 
 const routes: Routes = [
-  {path: 'contacts', component: ContactsCoreComponent},
+  {path: 'contacts', component: ContactsCoreComponent, canActivate: [ContactsGuard]},
   {path: 'schedule/:group', component: ScheduleCoreComponent},
 ];
 
---
+++ schedule/apps/main-spa/src/app/guard/contacts.guard.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {asyncScheduler, catchError, filter, Observable, scheduled, switchMap, tap} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {getAllContacts} from '../state/contacts/contacts.selectors';
+import {initContacts} from '../state/contacts/contacts.actions';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactsGuard implements CanActivate {
+
+  constructor(private store: Store) {
+  }
+
+  private checkStore(): Observable<boolean> {
+    return this.store.select(getAllContacts).pipe(
+      tap((contacts) => {
+        if (contacts.length == 0) {
+          this.store.dispatch(initContacts());
+        }
+      }),
+      filter((contacts) => contacts.length != 0),
+      map((contacts) => true),
+    );
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.checkStore().pipe(
+      catchError(() => scheduled([false], asyncScheduler)),
+    );
+  }
+
+}
