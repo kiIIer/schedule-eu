073d055ab4afee905987fe1934fbca00643dc7dc
kiIIer <39669501+kiIIer@users.noreply.github.com>
2022-11-20 16:01:08 +0100

    Contacts connection (#24)
    * remove test data
    
    * guard setup

--- schedule/apps/main-spa/src/app/app.component.ts
+++ schedule/apps/main-spa/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {Store} from '@ngrx/store';
-import {loadContactsSuccess} from './state/contacts/contacts.actions';
-import {testData} from './state/contacts/contacts.models';
 import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {viewHandsetOff, viewHandsetOn} from './state/view/view.actions';
 
@@ -14,9 +12,6 @@ export class AppComponent {
   title = 'main-spa';
 
   constructor(private store: Store, private breakpointObserver: BreakpointObserver) {
-    //TODO: remove test data
-    this.store.dispatch(loadContactsSuccess({contacts: testData}));
-
     this.breakpointObserver.observe(Breakpoints.Handset)
       .subscribe(((breakpoint) => breakpoint.matches ? this.store.dispatch(viewHandsetOn()) : this.store.dispatch(viewHandsetOff())));
   }
