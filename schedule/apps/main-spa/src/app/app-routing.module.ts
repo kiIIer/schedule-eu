bb446679abdf44dfe35c30af49e19b669e8ab8d2
killer <michael.molchanov.2004@gmail.com>
2022-11-21 14:36:47 +0100

    route setup

--- schedule/apps/main-spa/src/app/app-routing.module.ts
+++ schedule/apps/main-spa/src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import {StoreRouterConnectingModule} from '@ngrx/router-store';
 import {ContactsCoreComponent} from './core/contacts-core/contacts-core.component';
 import {ScheduleCoreComponent} from './core/schedule-core/schedule-core.component';
 import {ContactsGuard} from './guard/contacts/contacts.guard';
+import {FacultiesCoreComponent} from './core/faculties-core/faculties-core.component';
+import {FacultiesGuard} from './guard/faculties/faculties.guard';
 
 const routes: Routes = [
   {path: 'contacts', component: ContactsCoreComponent, canActivate: [ContactsGuard]},
+  {path: 'faculties', component: FacultiesCoreComponent, canActivate: [FacultiesGuard]},
   {path: 'schedule/:group', component: ScheduleCoreComponent},
 ];
 
