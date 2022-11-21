e2b321f0c454c49137df6c0b097f8d1d10a9d723
killer <michael.molchanov.2004@gmail.com>
2022-11-21 14:45:15 +0100

    removed test method

--- schedule/apps/main-spa/src/app/presentation/main-nav/main-nav.component.ts
+++ schedule/apps/main-spa/src/app/presentation/main-nav/main-nav.component.ts
@@ -1,10 +1,4 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
-import {Observable} from 'rxjs';
-import {map, shareReplay} from 'rxjs/operators';
-import {Store} from '@ngrx/store';
-import {initFaculties} from '../../state/faculties/faculties.actions';
-import {initContacts} from '../../state/contacts/contacts.actions';
 
 @Component({
   selector: 'schedule-main-nav',
@@ -14,12 +8,4 @@ import {initContacts} from '../../state/contacts/contacts.actions';
 export class MainNavComponent {
   @Output() goEvent: EventEmitter<string> = new EventEmitter<string>();
   @Input() isHandset: boolean | null = null;
-
-  constructor(private store: Store) {
-  }
-
-  test() {
-    this.store.dispatch(initContacts());
-  }
-
 }
