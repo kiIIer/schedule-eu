8e10fa40ca716c90ecaa9ffe28a7b2de43ec5273
killer <michael.molchanov.2004@gmail.com>
2022-11-21 14:26:38 +0100

    faculties component

---
+++ schedule/apps/main-spa/src/app/presentation/faculties/faculties.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'schedule-faculties',
+  templateUrl: './faculties.component.html',
+  styleUrls: ['./faculties.component.css'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class FacultiesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
