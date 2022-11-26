be4988703b6f032d104d0844e1ad5a48e727428a
killer <michael.molchanov.2004@gmail.com>
2022-11-26 19:44:05 +0100

    added faculty-search

--- schedule/apps/main-spa/src/app/presentation/faculties/faculties.component.ts
+++ schedule/apps/main-spa/src/app/presentation/faculties/faculties.component.ts
@@ -1,4 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable, startWith} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'schedule-faculties',
@@ -10,5 +13,22 @@ export class FacultiesComponent {
 
   @Input() facultyIds: string[] | null = [];
   @Output() goEvent: EventEmitter<string> = new EventEmitter<string>();
+
+  facultyControl = new FormControl('');
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.filteredOptions = this.facultyControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.filter(value || '')),
+    );
+  }
+
+  private filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    // eslint-disable-next-line
+    return this.facultyIds!.filter(option => option.toLowerCase().includes(filterValue));
+  }
 
 }
